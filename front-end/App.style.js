bf298c2221712ee6da222cd231b54620b3cad30b
drakenevadie19 <drakennievadje19@gmail.com>
2024-11-03 02:54:12 -0600

    New Change: Adding hot and cold background. Also adding ImageBackground to cover the whole app

--- front-end/App.style.js
+++ front-end/App.style.js
@@ -2,6 +2,15 @@ import { StyleSheet } from "react-native";
 
 export const s = StyleSheet.create({
     root: {
-        flex: 1
+        flex: 1,
+        justifyContent: "center"
+    },
+    workspace: {
+        height: 450,
+        alignItems: "center",
+        justifyContent: "space-evenly"
+    },
+    backgroundImage: {
+        height: "100%"
     }
 });
