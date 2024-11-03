18e7302affa2a790fef6813724aa5139ff46df8e
drakenevadie19 <drakennievadje19@gmail.com>
2024-11-03 03:34:20 -0600

    New Change: Adding and beautify input bar for converting

--- front-end/components/Input/Input.jsx
+++ front-end/components/Input/Input.jsx
@@ -7,7 +7,7 @@ const Input = ({defaultValue}) => {
       <TextInput
         style={s.input}
         placeholder="Type your temperature"
-        maxLength={4}
+        maxLength={3}
         defaultValue={defaultValue.toString()}
       />
       {/* Option + shift + 8 */}
